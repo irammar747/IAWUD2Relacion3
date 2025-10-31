c8959729347a6055962638e016ddb00a7d66bf60
Inma Ramirez <irammar747@g.educaand.es>
2025-10-31 17:12:46 +0100

    Añadida pequeña corrección

--- e6.php
+++ e6.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 5</title>
+    <title>Ejercicio 6</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
